233bb4ce14e12d0c1cf6dcf1514d05ab21bf9644
Mohamed Alaa El-Din <mohamed_alaa_el-din@hotmail.com>
2021-02-28 21:22:17 +0200

    add storage template

--- Storage.xctemplate/Userdefaults/StorageUserDefaultsManager/StorageUserDefaultsManagerKeys.swift
+++ Storage.xctemplate/Userdefaults/StorageUserDefaultsManager/StorageUserDefaultsManagerKeys.swift
@@ -1,17 +1,4 @@
 import Foundation
 
 struct StorageUserDefaultsManagerKeys {
-    enum GlobalKeys {
-        static let lastConnectionTime = "lastConnectionTime"
-        static let versionOfLastRun = "VersionOfLastRun"
-        static let currentVersion = "currentVersion"
-        static let isUserDimissCoachMarkView = "isUserDimissCoachMarkView"
-        static let shouldContactSupportHighlight = "shouldContactSupportHighlight"
-    }
-
-    enum LoginKeys {
-        static let token = "token"
-        static let refreshToken = "refreshToken"
-        static let refreshTokenExpirationDate = "refreshTokenExpirationDate"
-    }
 }
